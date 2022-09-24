cf475d615f15dce2d80a82adcaf1c9b28e1952b4
Alex Xu <12flawless.flaw@gmail.com>
2022-09-24 21:57:20 +1200

    Feature: added the player class

--- main.py
+++ main.py
@@ -2,6 +2,20 @@
 import pygame
 from sys import exit
 
+
+class Player(pygame.sprite.Sprite):
+    """Player class"""
+
+    def __init__(self):
+
+        super().__init__()
+        self.image = pygame.image.load(
+            "./assets/images/alien-player/alien_stand.png"
+        ).convert_alpha()
+
+        self.rect = self.image.get_rect(bottom=(ground_height))
+
+
 # Initialize pygame
 pygame.init()
 
@@ -17,6 +31,11 @@ sky_surf = pygame.image.load("./assets/images/sky.png").convert()
 ground_surf = pygame.image.load("./assets/images/ground.png").convert()
 ground_height = 300
 
+
+# Player elements
+player = pygame.sprite.GroupSingle()
+player.add(Player())
+
 # Primary game loop
 while True:
     # Event listener loop
@@ -28,6 +47,9 @@ while True:
     # Add backdrop to screen
     screen.blit(sky_surf, (0, 0))
     screen.blit(ground_surf, (0, ground_height))
+
+    # Player
+    player.draw(screen)
 
     # Game update frames
     pygame.display.update()
