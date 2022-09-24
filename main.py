c5fcf34d7e9c78d254235a6e75287ab6816278a6
Alex Xu <12flawless.flaw@gmail.com>
2022-09-24 21:41:26 +1200

    Features: Added backdrop elements
    - Added sky
    - Added ground

--- main.py
+++ main.py
@@ -2,12 +2,20 @@
 import pygame
 from sys import exit
 
+# Initialize pygame
+pygame.init()
+
 # Initial setup
-pygame.init
-screen_size = (800, 400)
-screen = pygame.display.set_mode((screen_size))
-clock = pygame.time.Clock()
+screen_width, screen_height = 800, 400
+screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Alien Run")
+clock = pygame.time.Clock()
+
+
+# Backdrop elements
+sky_surf = pygame.image.load("./assets/images/sky.png").convert()
+ground_surf = pygame.image.load("./assets/images/ground.png").convert()
+ground_height = 300
 
 # Primary game loop
 while True:
@@ -16,6 +24,10 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit
             exit()
+
+    # Add backdrop to screen
+    screen.blit(sky_surf, (0, 0))
+    screen.blit(ground_surf, (0, ground_height))
 
     # Game update frames
     pygame.display.update()
