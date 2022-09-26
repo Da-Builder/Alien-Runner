b3555c46a01d6cf73248d680127dc9e8a35a4da4
Alex Xu <12flawless.flaw@gmail.com>
2022-09-26 22:27:39 +1300

    Broken snail updated gitignore

--- main.py
+++ main.py
@@ -1,26 +1,27 @@
 # Import modules
-import pygame
+import pygame as pg
 from sys import exit
+from random import randint
 
 
-class Player(pygame.sprite.Sprite):
+class Player(pg.sprite.Sprite):
     """Player class"""
 
     def __init__(self):
 
         super().__init__()
-        self.image = pygame.image.load(
+        self.image = pg.image.load(
             "./assets/images/alien-player/alien_stand.png"
         ).convert_alpha()
 
-        self.rect = self.image.get_rect(midbottom=(50, ground_height))
+        self.rect = self.image.get_rect(midbottom=(80, ground_height))
         self.gravity = 0
 
     def user_input(self):
-        keys = pygame.key.get_pressed()
-        mouse = pygame.mouse.get_pressed()
+        keys = pg.key.get_pressed()
+        mouse = pg.mouse.get_pressed()
 
-        if keys[pygame.K_SPACE] and self.rect.bottom >= ground_height or mouse[0] and self.rect.bottom >= ground_height:
+        if keys[pg.K_SPACE] and self.rect.bottom >= ground_height or mouse[0] and self.rect.bottom >= ground_height:
             self.gravity = -15
 
     def apply_gravity(self):
@@ -38,42 +39,82 @@ class Player(pygame.sprite.Sprite):
         self.apply_gravity()
 
 
-        # Initialize pygame
-pygame.init()
+class Mobs(pg.sprite.Sprite):
+    """Mobs class fro all enemy"""
+
+    def __init__(self, image, speed=(5, 10), height=0):
+
+        super().__init__()
+        self.image = pg.image.load(
+            "./assets/images/snail-mob/snail_1.png").convert_alpha()
+        self.rect = self.image.get_rect(
+            bottomleft=(800, ground_height + height)
+        )
+
+        self.speed = randint(speed[0], speed[1])
+
+    def travel(self):
+        self.rect.x += self.speed
+
+        if self.rect.right <= 0:
+            self.rect.x = screen_width
+
+    def animation(self):
+        pass
+
+    def update(self):
+        self.travel()
+
+
+# Initialize pygame
+pg.init()
 
 # Initial setup
 screen_width, screen_height = 800, 400
-screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption("Alien Run")
-clock = pygame.time.Clock()
+screen = pg.display.set_mode((screen_width, screen_height))
+pg.display.set_caption("Alien Run")
+clock = pg.time.Clock()
 
 
 # Backdrop elements
-sky_surf = pygame.image.load("./assets/images/sky.png").convert()
-ground_surf = pygame.image.load("./assets/images/ground.png").convert()
+sky_surf = pg.image.load("./assets/images/sky.png").convert()
+ground_surf = pg.image.load("./assets/images/ground.png").convert()
 ground_height = 300
 
 
 # Player elements
-player = pygame.sprite.GroupSingle()
+player = pg.sprite.GroupSingle()
 player.add(Player())
+
+
+# Mob elements
+snail = pg.sprite.GroupSingle()
+snail.add(Mobs(
+    image=pg.image.load(
+        "./assets/images/snail-mob/snail_1.png").convert_alpha()
+))
+
 
 # Primary game loop
 while True:
     # Event listener loop
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            pg.quit
             exit()
 
     # Add backdrop to screen
     screen.blit(sky_surf, (0, 0))
     screen.blit(ground_surf, (0, ground_height))
 
+    # Snail
+    snail.draw(screen)
+    snail.update()
+
     # Player
     player.draw(screen)
     player.update()
 
     # Game update frames
-    pygame.display.update()
+    pg.display.update()
     clock.tick(60)
